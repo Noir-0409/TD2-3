0e43ed08ed52904d12f71695313e15e118809a67
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2025-02-05 22:45:22 +0900

    終了

--- KamataEngine/DirectXGame/main.cpp
+++ KamataEngine/DirectXGame/main.cpp
@@ -52,7 +52,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow(L"2344_惑星探索");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
